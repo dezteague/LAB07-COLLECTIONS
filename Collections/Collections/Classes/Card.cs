7ae6e81080770fc0c16028dd5cba1f68b1bbcf9c
Deziree' Teague <dez.teague@gmail.com>
2019-01-16 13:50:13 -0800

    add enums

--- Collections/Collections/Classes/Card.cs
+++ Collections/Collections/Classes/Card.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 namespace Collections.Classes
 {
-
+    
     class Card
     {
+        enum Suit { Hearts, Spades, Diamonds, Clubs}
+        enum value { one, two, three, four, five, six, seven, eight, nine, ten, jack, king, queen, ace}
     }
 }
